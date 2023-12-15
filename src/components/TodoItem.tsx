611c4600d84c5b5bf8cbac17f26ecb53c6bb42db
j-yesung <hisung-ah@kakao.com>
2023-12-15 11:11:41 +0900

    add Swal confirm

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,23 +1,12 @@
+import { useConfirm } from '../hooks/useConfirm';
 import { useTodos } from '../hooks/useTodos';
 import * as S from '../styles/TodoList.styled';
 import 'react-toastify/dist/ReactToastify.css';
-import { toast } from 'react-toastify';
 
-interface ItemProps {
-  todo: Todo;
-  isLoading: boolean;
-}
-
-export const TodoItem = ({ todo, isLoading }: ItemProps) => {
+export const TodoItem = ({ todo, isLoading }: { todo: Todo; isLoading: boolean }) => {
   const { id, title, content, isDone } = todo;
-  const { updateTodos, deleteTodos } = useTodos();
-
-  const deleteTodoHandler = () => {
-    if (window.confirm('정말로 삭제하시겠습니까?')) {
-      deleteTodos(id);
-      toast.success('삭제되었습니다.');
-    }
-  };
+  const { updateTodos } = useTodos();
+  const { checkDeleteTodo } = useConfirm(id);
 
   if (isLoading) return <div>로딩 중입니다...</div>;
 
@@ -30,7 +19,7 @@ export const TodoItem = ({ todo, isLoading }: ItemProps) => {
           <S.Button onClick={() => updateTodos(todo)} color="#0abd00">
             {isDone ? '취소' : '완료'}
           </S.Button>
-          <S.Button onClick={deleteTodoHandler} color="#ff4444">
+          <S.Button onClick={checkDeleteTodo} color="#ff4444">
             삭제
           </S.Button>
         </S.ButtonBox>
