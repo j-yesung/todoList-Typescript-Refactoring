4581937de6ad0ad7beb2f9202c0b5538597a59f0
j-yesung <hisung-ah@kakao.com>
2023-12-21 16:51:35 +0900

    tanstack-query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TodoList } from './components/TodoList';
 import { Layout } from './layout/Layout';
 import { GlobalStyle } from './styles/GlobalStyle';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
         <GlobalStyle />
         <Layout>
           <TodoForm />
